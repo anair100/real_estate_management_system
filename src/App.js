4b37559f1306a6ab074c6076d30f670c0d4e45fd
songareravi <ravi.songare@outlook.com>
2025-01-03 22:44:31 +0530

    react bootstrap added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Footer from './Components/Footer';
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
