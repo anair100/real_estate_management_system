af7c95150e2b063492dc8b69dea2e668ee6119f8
anair100 <adityanair2206@outlook.com>
2025-01-14 10:43:17 +0530

    Added images in Search Result Pages

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -129,7 +129,7 @@ import { useNavigate } from 'react-router';
   };
 
   return (
-      <div className="main-content">
+      <div style = {{textAlign: "center", maxWidth: "100%", width: "100%", marginBottom: "0", backgroundColor: "#F1F2F2", padding: "0"}}>
        <div style={{ backgroundColor: isMobile? "#96E3E4": "#96E3E4", padding: "20px" }}>
           <Image style={{ width: isMobile? "50%": "25%", height: "auto" }} src={home_image} />
        </div>
@@ -151,7 +151,7 @@ import { useNavigate } from 'react-router';
             checked={formData.rentOrSell === 'Sell'} onChange={handleChange}
             style={{ appearance: "none", width: isMobile? "1.5vw": "1vw", height:  isMobile? "1.5vw": "1vw", border: "0.2vw solid black", borderRadius: "50%", 
               outline: "none", cursor: "pointer",  transition: "background-color 0.3s", borderColor: "0.3s", marginRight: "0.5vw", marginLeft: isMobile? "5vw": "3vw"}}
-           />Sell
+           />Buy
           </label>
           <label style={{fontSize: isMobile? "4vw": "2vw", display: "flex", justifyContent: "center", alignItems: "center", cursor: "pointer", fontWeight: "500", 
            width: "50%", borderWidth: "0.1vw", borderColor: "#ccc",  padding: "0vw", boxSizing: "border-box", }}>
--- src/Components/SearchResult.js
+++ src/Components/SearchResult.js
@@ -5,6 +5,7 @@ import home_image from '../resources/home_image.webp';
 // import api from '../api/api';
 
 const SearchResult = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
   const [properties, setProperties] = useState([]);
   const [searchParams] = useSearchParams();
   console.log("searchParams")
@@ -61,31 +62,52 @@ const SearchResult = () => {
 
   return (
     <div style = {{backgroundColor: "#F1F2F2"}}>
-      <div style = {{backgroundColor: "#F2FCFF", width: "100%", height: "auto", padding: "1vw 1vw"}}>
+      <div style = {{backgroundColor: "#F2FCFF", width: "100%", height: "auto",  display: "flex", flexDirection: "column", alignItems: "flex-start", gap: "1vw"}}>
+      <label>
+       Looking for:
+       <select style={{ marginLeft: "1vw", padding: "0.5vw", borderRadius: "5px" }}>
+        <option value="buy">Buy</option>
+        <option value="rent">Rent</option>
+       </select>
+      </label>
       <button style={{ backgroundColor: 'blue', color: 'white', border: 'none', borderRadius: '5px', 
         padding: '1vw 1vw', borderRadius: "5vw", marginRight: "0"}}>Modify Search</button>
       </div>
-      <div style = {{marginLeft: "11vw", fontSize: "2vw", marginTop: "2vw", padding: "0"}}>Showing 2 of 5</div>
-      <div style = {{marginLeft: "11vw", fontSize: "3vw", marginTop: "0", padding: "0", borderStyle: "solid", 
-        marginRight: "0.5vw", marginBottom: "1vw", width: "80%"}}>Projects In {searchParams.get("location")}</div>
+
+      <div style = {{marginLeft: "11vw", fontSize: "3vw", marginTop: "0", padding: "0", marginRight: "0.5vw", 
+        marginBottom: "1.2vw", width: "80%"}}>
+        <h1 style = {{fontSize: "2vw", marginTop: "2vw", padding: "0", marginBottom: "0"}}>Showing 2 of 5</h1>
+        <h2 style = {{fontSize: "3vw", marginTop: "0", padding: "0", marginBottom: "1.2vw"}}>Projects In {searchParams.get("location")}</h2>
+      </div>
 
       <ul style = {{width: "80%", margin: "auto", position: "relative", height: "auto", padding: "0", display: "block"}}>
        {properties.map((property) => (
         <div style = {{borderStyle: "hidden", borderRadius: "2vw", width: "100%", height: "auto", backgroundColor: "#FAFEFF", 
           display: "flex", marginBottom: "1vw", marginTop: "0", padding: "1vw 1vw"}}>               
-         <img style = {{width: "50%"}} src={`http://localhost:8080/${property.images[0]}`}
-                    alt={`Property`}/>
+         <img style = {{width: "40%"}} src={`http://localhost:8080/${property.images[0]}`} alt={`Property`}/>
          <div style = {{marginBottom: "0", display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent: "flex-start",
-          padding: "0", width: "80%", height: "100%", marginRight: "0vw", backgroundColor: "#FAFEFF"}}>  
-          <p class="cgal-info-header-text">
-           {property.type}
-          </p>
-          <p class="cgal-info-desc-text">
-           {property.location}
-          </p>
-          <button style={{ backgroundColor: 'red', color: 'white', border: 'none', borderRadius: '5px', padding: '5px 10px', marginRight: '10px' }}>Contact</button>
-          <button style={{ backgroundColor: 'green', color: 'white', border: 'none', borderRadius: '5px', padding: '5px 10px' }}>Whatsapp</button>
+          padding: "0", width: "80%", height: "100%", marginRight: "0vw", marginLeft: "1vw",backgroundColor: "#FAFEFF", width: "50%"}}>  
+          <div style = {{fontSize: "2vw", fontWeight: "600"}}>
+           {property.size} {property.type} For {property.rentOrSell} In {property.location}
           </div>
+          <div style = {{fontSize: "2vw", fontWeight: "600"}}>
+           <div style = {{fontSize: "2vw", fontWeight: "600"}}>
+             Area: {property.size}
+           </div>
+           <div style = {{fontSize: "2vw", fontWeight: "600"}}>
+             Price: {property.price}
+           </div>
+          </div>
+          <div style = {{ display: "block", maxWidth: "100%", margin: "auto", marginTop: "0.5vw",
+            marginBottom: "0.5vw", borderStyle: "solid", width: "60%"}}>
+           <a style = {{padding: "1vw", backgroundColor: "#25D366", color: "#fff", textDecoration: "none", 
+           fontFamily: "sans-serif", fontSize: "1.5vw", float: "right", marginLeft: "3vw"}} href="https://api.whatsapp.com/send?phone=9981069233">
+            WhatsApp
+           </a>
+           <a style = {{padding: "1vw", backgroundColor: "blue", color: "#fff", textDecoration: "none",
+            fontFamily: "sans-serif", fontSize: "1.5vw", float: "left", marginRight: "1vw"}} href="tel:9981069233">Contact</a>
+          </div> 
+         </div>
         </div>))}
       </ul>
     </div>
