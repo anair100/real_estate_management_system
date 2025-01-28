2c1beee7527505e26561ca11ad2bb18b198228c9
songareravi <ravi.songare@outlook.com>
2025-01-28 23:08:06 +0530

    UI changes for Local

--- src/Components/api.js
+++ src/Components/api.js
@@ -1,11 +1,12 @@
 
 import axios from 'axios';
 
-// const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+ const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 // console.log('baseUrl:',apiBaseUrl);
 
 const api = axios.create({
+  //Uncomment for seperate UI and service in local
   // baseURL: apiBaseUrl,
 });
 
