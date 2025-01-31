ded4f587f67125900208d6ea27c64b07194091bd
anair100 <adityanair2206@outlook.com>
2025-01-31 09:52:48 +0530

    Home_Page_Mobile_View_Changes_5

--- src/Components/AddPropertyForm.js
+++ src/Components/AddPropertyForm.js
@@ -8,12 +8,12 @@ import api from './api';
 const AddPropertyForm = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
   useEffect(() => {
-      const handleResize = () => {
-        setIsMobile(window.innerWidth <= 768);
-  };
-  window.addEventListener("resize", handleResize);
-  return () => {
-    window.removeEventListener("resize", handleResize);
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+     window.removeEventListener("resize", handleResize);
   }},[]);
   const radioButtons = document.querySelectorAll('input[type="radio"]');
   radioButtons.forEach((radio) => {
@@ -22,7 +22,6 @@ const AddPropertyForm = () => {
         btn.style.backgroundColor = ''; // Reset background
         btn.style.borderColor = '#black';
       });
-
       if (radio.checked) {
         radio.style.backgroundColor = '#007BFF'; // Highlight background
         radio.style.borderColor = '#black'; // Darker border
@@ -70,26 +69,16 @@ const AddPropertyForm = () => {
         }
       }
     }
-    // 
     
     console.log("Inspecting formData:" ,formData);
     console.log("Inspecting data:" ,data);
-for (let [key, value] of data.entries()) {
-  console.log(key, value);
-}
+    for (let [key, value] of data.entries()) {
+     console.log(key, value);
+    }
 
     try {
       console.log('data',data);
-    //  const response = await fetch('http://localhost:8080/api/properties/add',{
-    //     method :'POST',
-    //     body:data,
-    //     // headers:{
-    //     //   'Content-Type':'application/json'
-    //     // }
-    //   })
-      const response  = await api.post('/api/properties/add', data);
-      
-
+      const response  = await api.post('http://localhost:8080/api/properties/add', data);
       alert('Property added successfully');
       console.log( response);
     } catch (error) {
@@ -103,12 +92,13 @@ for (let [key, value] of data.entries()) {
     <div style={{ backgroundColor: isMobile? "#96E3E4": "#96E3E4", padding: "20px" }}>
        <Image style={{ width: isMobile? "50%": "25%", height: "auto" }} src={home_image} />
     </div>
-    <Container style={{width: isMobile ? "90%" : "50%", marginTop: "2vh", borderStyle: "hidden", borderRadius: "20px", padding: "0",
-     width: "50%", height: "auto", backgroundColor: "#FAFEFF", marginLeft: "10vw", marginRight: "10vw", position: "relative",
-     marginTop: "3vh"}}>
-    <div style = {{ display: "flex", width: "80%", marginLeft: "2vw", marginBottom: isMobile? "2vw": "1vw", marginTop: "0"}}>
+    <Container className="input-container" style={{ width: isMobile ? "90%" : "50%", marginTop: isMobile? "5vw": "2vh", paddingTop: isMobile? "3vw": "1vw",
+      borderStyle: "hidden", borderRadius: "20px", height: "auto", backgroundColor: "#FAFEFF"}}>
+       <h1 style={{fontSize: "3vw", width: "100%", marginBottom: isMobile ? "2.5vw" : "1.2vw", padding: "0vw"}}>
+          Add Property</h1>
+       <div style = {{ display: "flex", width: "80%", marginLeft: "2vw", marginBottom: isMobile? "2vw": "1vw", marginTop: "0"}}>
          <div style={{ fontSize: isMobile? "4vw": "2vw", display: "flex", alignItems: "center", padding: "0", fontWeight: "500"}}>
-            Looking For:</div>
+            Looking To:</div>
          <div style={{display: "flex", padding: "0", margin: "0", width: "50%"}}>
           <label style = {{fontSize: isMobile? "4vw": "2vw", display: "flex", justifyContent: "center", alignItems: "center", cursor: "pointer", fontWeight: "500", 
            width: "50%", borderWidth: "0.1vw", borderColor: "#ccc",  padding: "0vw", boxSizing: "border-box"}}>
@@ -126,20 +116,41 @@ for (let [key, value] of data.entries()) {
            />Rent
           </label>
          </div>
-        </div>
-      <input name="type" placeholder="Type" value={formData.type} onChange={handleChange} required />
-      <input name="price" placeholder="Price" value={formData.price} onChange={handleChange} required />
-      <input name="size" placeholder="Size" value={formData.size} onChange={handleChange} required />
-      <input name="phone" placeholder="Phone" value={formData.phone} onChange={handleChange} required />
-      <input name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
+       </div>
+       <div style = {{ display: "flex", width: "80%", marginLeft: "2vw", marginBottom: isMobile? "2vw": "1vw", marginTop: "0"}}>
+        <div style={{ fontSize: isMobile? "4vw": "2vw", display: "flex", alignItems: "center", padding: "0", fontWeight: "500"}}>
+         Property Type:</div>
+        <input name="type" placeholder="Type" value={formData.type} onChange={handleChange} required />
+       </div>
+       <div style = {{ display: "flex", width: "80%", marginLeft: "2vw", marginBottom: isMobile? "2vw": "1vw", marginTop: "0"}}>
+        <div style={{ fontSize: isMobile? "4vw": "2vw", display: "flex", alignItems: "center", padding: "0", fontWeight: "500"}}>
+         Locality:</div>
+         <input name="location" placeholder="Location" value={formData.location} onChange={handleChange} required
+         style = {{borderRadius: '1vw', borderStyle: "ridge", borderStyle: "ridge", borderColor: "#ddd", fontSize: isMobile? "4vw": "2vw"}}/>
+       </div>
+       <div style = {{ display: "flex", width: "80%", marginLeft: "2vw", marginBottom: isMobile? "2vw": "1vw", marginTop: "0"}}>
+        <div style={{ fontSize: isMobile? "4vw": "2vw", display: "flex", alignItems: "center", padding: "0", fontWeight: "500"}}>
+         Size:</div>
+        <input name="size" placeholder="Enter Size" value={formData.size} onChange={handleChange} required 
+         style = {{borderRadius: '1vw', borderStyle: "ridge", borderStyle: "ridge", borderColor: "#ddd", fontSize: isMobile? "4vw": "2vw"}}/>
+       </div>
+       <div>
+        <input name="price" placeholder="Price" value={formData.price} onChange={handleChange} required />
+       </div>
+       <div>
+        <input name="phone" placeholder="Phone" value={formData.phone} onChange={handleChange} required />
+       </div>
+      
       <textarea name="description" placeholder="Description" value={formData.description} onChange={handleChange} required></textarea>
-      <input name="location" placeholder="Location" value={formData.location} onChange={handleChange} required />
-      <input name="googleLocation" placeholder="Google Location" value={formData.googleLocation} onChange={handleChange} required />
       <label>Images:</label>
       <input type="file" name="images" multiple onChange={handleChange} />
       <label>Videos:</label>
       <input type="file" name="videos" multiple onChange={handleChange} />
-      <button onClick={handleSubmit} type="submit">Add Property</button>
+      <div style={{ marginTop: isMobile ? "12vw" : "4vw", display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "0vw", padding: "0" }}>
+          <button style={{backgroundColor: "#B9B8E3", borderRadius: isMobile? "7vw": "5vw", fontSize: isMobile ? "5vw" : "1.5vw", 
+          fontWeight: "500", padding: isMobile ? "2vw 10vw" : "0.8vw 5vw", border: "none"
+          }} onClick={handleSubmit} type="submit">Add</button>
+      </div>
     </Container>
     </div>
   );
--- src/Components/api.js
+++ src/Components/api.js
@@ -2,12 +2,11 @@
 import axios from 'axios';
 
  const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
-
 // console.log('baseUrl:',apiBaseUrl);
 
 const api = axios.create({
   //Uncomment for seperate UI and service in local
-  // baseURL: apiBaseUrl,
+  //baseURL: apiBaseUrl,
 });
 
 export default api;
